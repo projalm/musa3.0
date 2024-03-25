8b899c03414296ac89bc2cdbc98f3a9e576789e9
Jose Leon <alejandro_leon9412@hotmail.com>
2024-03-25 08:14:56 -0500

    Adding dialogue

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const {
   createProvider,
   createFlow,
   addKeyword,
+  addAnswer,
 } = require("@bot-whatsapp/bot");
 
 const MetaProvider = require("@bot-whatsapp/provider/meta");
@@ -20,6 +21,8 @@ const MockAdapter = require("@bot-whatsapp/database/mock");
  *
  * Primero declaras los submenus 1.1 y 2.1, luego el 1 y 2 y al final el principal.
  */
+
+let dataUser;
 
 const flowSecundario = addKeyword(["2", "siguiente"]).addAnswer([
   "📄 Aquí tenemos el flujo secundario",
@@ -64,6 +67,97 @@ const flowGracias = addKeyword(["gracias", "grac"]).addAnswer(
   [flowSecundario]
 );
 
+const flowWelcome = addKeyword("hola")
+  .addAnswer(
+    "🌈 ¡En este viaje de aprendizaje, estaré siempre a tu lado! 🐾💃 Soy uno de los personajes de Musa, pero en esta ocasión, llevo los colores de *atingi*. 🌟💼 🐾💃 \n\n¿Por qué me estoy pintando de los colores de atingi? Bueno, es porque en este viaje educativo, Musa y atingi se han unido para brindarte lo mejor de ambos mundos: la creatividad y la diversión de Musa con la potencia y el conocimiento de atingi. 🌟💼",
+    {
+      // media: "https://i.imgur.com/0HpzsEm.png",
+      buttons: [{ body: "¡GENIAL!" }],
+      capture: true,
+    }
+  )
+  .addAnswer(
+    "📱 Para que estemos más conectad@s, no olvides guardar este número en tus contactos. Ah, y por cierto, *¿cómo te llamas?* Me encantaría conocerte mejor. 👀",
+    {
+      // media: "https://i.imgur.com/0HpzsEm.png",
+      capture: true,
+    },
+    async (ctx, { flowDynamic, state }) => {
+      await state.update({ name: ctx.body });
+      dataUser = state.get("name");
+      // await flowDynamic(
+      //   `😁 ¡Muchas gracias, ${myState.name}! A partir de ahora, usaré ese nombre para dirigirme a ti. ¿Te parece bien?`,
+      //   {
+      //     // media: "https://i.imgur.com/0HpzsEm.png",
+      //     buttons: [{ body: "SI" }, { body: "NO" }],
+      //     capture: true,
+      //   }
+      // );
+    }
+  )
+  .addAnswer(
+    `😁 ¡Muchas gracias, ${dataUser}! A partir de ahora, usaré ese nombre para dirigirme a ti. ¿Te parece bien?`,
+    {
+      // media: "https://i.imgur.com/0HpzsEm.png",
+      buttons: [{ body: "SI!" }, { body: "NO" }],
+      capture: true,
+    },
+    async (ctx, { flowDynamic, gotoFlow }) => {
+      const acceptName = ctx.body;
+
+      if (acceptName === "SI") {
+        await gotoFlow(rejectedName);
+      }
+    }
+  )
+  .addAnswer(
+    "¡Excelente! 😄 Ahora, tengo dos preguntas para registrar tus datos y poder enviarte tu constancia de participación al finalizar esta sesión de aprendizaje.",
+    {
+      // media: "https://i.imgur.com/0HpzsEm.png",
+      buttons: [{ body: "OK" }],
+      capture: true,
+    }
+  )
+  .addAnswer(
+    "💁‍♂️💁‍♀️ Dime, ¿con qué género te identificas?\n\n*A.* Femenino\n*B.* Masculino\n*C.* Otro\n*D.* Prefiero no decirlo\n\n✍️ *Escribe una letra entre A y D.*",
+    {
+      // media: "https://i.imgur.com/0HpzsEm.png",
+      capture: true,
+    }
+  )
+  .addAnswer(
+    "😊💬 ¡Qué emoción! Ahora que conozco un poco más sobre ti, estoy feliz de embarcarnos juntos en esta emocionante aventura de aprendizaje",
+    {
+      // media: "https://i.imgur.com/0HpzsEm.png",
+      buttons: [{ body: "¡EMPECEMOS!" }],
+      capture: true,
+    }
+  )
+  .addAnswer(
+    "Claro que sí. ¡Bienvenid@ a esta sesión!",
+    {
+      // media: "https://i.imgur.com/0HpzsEm.png",
+    },
+    null,
+    async (ctx, { flowDynamic, gotoFlow }) => {
+      await gotoFlow(mainFlow);
+    }
+  );
+
+const mainFlow = addKeyword("MAIN_FLOW")
+  .addAnswer("*EXPLORANDO EL MUNDO DEL MARKETING DIGITAL* 🌐")
+  .addAnswer(
+    "⏱️ *Duración promedio de la sesión* \n10 minutos\n\n🤖 *Tip atingi:* \n▪️Puedes retomar el aprendizaje en cualquier momento\n\n📧 *Correo para consultas:* \nsoporte@holamusa.com",
+    { buttons: [{ body: "OK" }] }
+  );
+
+const rejectedName = addKeyword("REJECTED_NAME").addAnswer(
+  "🤔 No te preocupes, ¡lo resolveremos juntos! Por favor, proporciona tu nombre nuevamente para asegurarnos de que esté correcto esta vez. 😊👍",
+  { capture: true },
+  null,
+  [flowWelcome]
+);
+
 const flowDiscord = addKeyword(["discord"]).addAnswer(
   [
     "🤪 Únete al discord",
@@ -75,7 +169,7 @@ const flowDiscord = addKeyword(["discord"]).addAnswer(
   [flowSecundario]
 );
 
-const flowPrincipal = addKeyword(["hola", "ole", "alo"])
+const flowPrincipal = addKeyword(["principal"])
   .addAnswer("🙌 Hola bienvenido a este *Chatbot*")
   .addAnswer(
     [
@@ -91,13 +185,13 @@ const flowPrincipal = addKeyword(["hola", "ole", "alo"])
 
 const main = async () => {
   const adapterDB = new MockAdapter();
-  const adapterFlow = createFlow([flowPrincipal]);
+  const adapterFlow = createFlow([flowWelcome]);
 
   const adapterProvider = createProvider(MetaProvider, {
     jwtToken:
       "EAAO7cfcvkY4BOz5D0QqZCQMYEGYs7ZB5mmsxBbdjDqAiB1VXeWAI5ujXHqaPNv8Xixqyjw01YeKZC5BEAwZCpQwAarx9Mip1o4wFinEq3bLmz1znmXZBXIVMGbpXMiXjloJszmnwk09eFc8ShIY2CN6fx2HBz5ry3ZBgV9MXNrmRHdK19BEvdifNpFvEa78G9FaXfOmKhm16jHl00ZD",
     numberId: "244396062094814",
-    verifyToken: "LOQUESEA",
+    verifyToken: "loquesea",
     version: "v16.0",
   });
 
