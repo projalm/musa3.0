7746118cd7d0ae04440d1e9aac8adf9fbb120bae
Jose Leon <alejandro_leon9412@hotmail.com>
2024-03-26 16:02:16 -0500

    gotoFlow

--- app.js
+++ app.js
@@ -22,113 +22,150 @@ const MockAdapter = require("@bot-whatsapp/database/mock");
  * Primero declaras los submenus 1.1 y 2.1, luego el 1 y 2 y al final el principal.
  */
 
-let dataUser;
+// const flowSecundario = addKeyword(["2", "siguiente"]).addAnswer([
+//   "📄 Aquí tenemos el flujo secundario",
+// ]);
 
-const flowSecundario = addKeyword(["2", "siguiente"]).addAnswer([
-  "📄 Aquí tenemos el flujo secundario",
-]);
+// const flowDocs = addKeyword([
+//   "doc",
+//   "documentacion",
+//   "documentación",
+// ]).addAnswer(
+//   [
+//     "📄 Aquí encontras las documentación recuerda que puedes mejorarla",
+//     "https://bot-whatsapp.netlify.app/",
+//     "\n*2* Para siguiente paso.",
+//   ],
+//   null,
+//   null,
+//   [flowSecundario]
+// );
 
-const flowDocs = addKeyword([
-  "doc",
-  "documentacion",
-  "documentación",
-]).addAnswer(
-  [
-    "📄 Aquí encontras las documentación recuerda que puedes mejorarla",
-    "https://bot-whatsapp.netlify.app/",
-    "\n*2* Para siguiente paso.",
-  ],
-  null,
-  null,
-  [flowSecundario]
-);
+// const flowTuto = addKeyword(["tutorial", "tuto"]).addAnswer(
+//   [
+//     "🙌 Aquí encontras un ejemplo rapido",
+//     "https://bot-whatsapp.netlify.app/docs/example/",
+//     "\n*2* Para siguiente paso.",
+//   ],
+//   null,
+//   null,
+//   [flowSecundario]
+// );
 
-const flowTuto = addKeyword(["tutorial", "tuto"]).addAnswer(
-  [
-    "🙌 Aquí encontras un ejemplo rapido",
-    "https://bot-whatsapp.netlify.app/docs/example/",
-    "\n*2* Para siguiente paso.",
-  ],
-  null,
-  null,
-  [flowSecundario]
-);
+// const flowGracias = addKeyword(["gracias", "grac"]).addAnswer(
+//   [
+//     "🚀 Puedes aportar tu granito de arena a este proyecto",
+//     "[*opencollective*] https://opencollective.com/bot-whatsapp",
+//     "[*buymeacoffee*] https://www.buymeacoffee.com/leifermendez",
+//     "[*patreon*] https://www.patreon.com/leifermendez",
+//     "\n*2* Para siguiente paso.",
+//   ],
+//   null,
+//   null,
+//   [flowSecundario]
+// );
 
-const flowGracias = addKeyword(["gracias", "grac"]).addAnswer(
-  [
-    "🚀 Puedes aportar tu granito de arena a este proyecto",
-    "[*opencollective*] https://opencollective.com/bot-whatsapp",
-    "[*buymeacoffee*] https://www.buymeacoffee.com/leifermendez",
-    "[*patreon*] https://www.patreon.com/leifermendez",
-    "\n*2* Para siguiente paso.",
-  ],
-  null,
-  null,
-  [flowSecundario]
-);
-
-const flowWelcome = addKeyword("hola")
+const mainFlow = addKeyword("MAIN_FLOW")
+  .addAnswer("*EXPLORANDO EL MUNDO DEL MARKETING DIGITAL* 🌐")
   .addAnswer(
-    "🌈 ¡En este viaje de aprendizaje, estaré siempre a tu lado! 🐾💃 Soy uno de los personajes de Musa, pero en esta ocasión, llevo los colores de *atingi*. 🌟💼 🐾💃 \n\n¿Por qué me estoy pintando de los colores de atingi? Bueno, es porque en este viaje educativo, Musa y atingi se han unido para brindarte lo mejor de ambos mundos: la creatividad y la diversión de Musa con la potencia y el conocimiento de atingi. 🌟💼",
+    "⏱️ *Duración promedio de la sesión* \n10 minutos\n\n🤖 *Tip atingi:* \n▪️Puedes retomar el aprendizaje en cualquier momento\n\n📧 *Correo para consultas:* \nsoporte@holamusa.com",
+    { capture: true, buttons: [{ body: "OK" }] }
+  )
+  .addAnswer(
+    "🚀 ¡Cuando concluyas esta sesión, estarás listo para comenzar a implementar estrategias y aumentar la presencia digital de tu negocio! 📈📊\n\n✅ Y recibirás tu constancia de participación 🏆📜.",
     {
-      // media: "https://i.imgur.com/0HpzsEm.png",
+      capture: true,
       buttons: [{ body: "¡GENIAL!" }],
-      capture: true,
     }
-  )
-  .addAnswer(
-    "📱 Para que estemos más conectad@s, no olvides guardar este número en tus contactos. Ah, y por cierto, *¿cómo te llamas?* Me encantaría conocerte mejor. 👀",
-    {
-      // media: "https://i.imgur.com/0HpzsEm.png",
-      capture: true,
-    },
-    async (ctx, { flowDynamic, state }) => {
-      await state.update({ name: ctx.body });
-      dataUser = state.get("name");
-      // await flowDynamic(
-      //   `😁 ¡Muchas gracias, ${myState.name}! A partir de ahora, usaré ese nombre para dirigirme a ti. ¿Te parece bien?`,
-      //   {
-      //     // media: "https://i.imgur.com/0HpzsEm.png",
-      //     buttons: [{ body: "SI" }, { body: "NO" }],
-      //     capture: true,
-      //   }
-      // );
-    }
-  )
-  .addAnswer(
-    `😁 ¡Muchas gracias, ${dataUser}! A partir de ahora, usaré ese nombre para dirigirme a ti. ¿Te parece bien?`,
-    {
-      // media: "https://i.imgur.com/0HpzsEm.png",
-      buttons: [{ body: "SI!" }, { body: "NO" }],
-      capture: true,
-    },
-    async (ctx, { flowDynamic, gotoFlow }) => {
-      const acceptName = ctx.body;
+  );
 
-      if (acceptName === "SI") {
-        await gotoFlow(require("./app/rejectedName"));
-      }
-    }
-  )
+const afterTakeName = addKeyword("AFTER_TAKE_NAME")
   .addAnswer(
     "¡Excelente! 😄 Ahora, tengo dos preguntas para registrar tus datos y poder enviarte tu constancia de participación al finalizar esta sesión de aprendizaje.",
     {
-      // media: "https://i.imgur.com/0HpzsEm.png",
-      buttons: [{ body: "OK" }],
       capture: true,
+      buttons: [{ body: "OK" }],
     }
   )
   .addAnswer(
     "💁‍♂️💁‍♀️ Dime, ¿con qué género te identificas?\n\n*A.* Femenino\n*B.* Masculino\n*C.* Otro\n*D.* Prefiero no decirlo\n\n✍️ *Escribe una letra entre A y D.*",
     {
-      // media: "https://i.imgur.com/0HpzsEm.png",
       capture: true,
     }
   )
   .addAnswer(
     "😊💬 ¡Qué emoción! Ahora que conozco un poco más sobre ti, estoy feliz de embarcarnos juntos en esta emocionante aventura de aprendizaje",
     {
-      // media: "https://i.imgur.com/0HpzsEm.png",
+      capture: true,
+      buttons: [{ body: "¡EMPECEMOS!" }],
+    }
+  )
+  .addAnswer(
+    "Claro que sí. ¡Bienvenid@ a esta sesión!",
+    null,
+    async (ctx, { gotoFlow }) => {
+      if (ctx.body) {
+        return gotoFlow(mainFlow);
+      }
+    },
+    [mainFlow]
+  );
+
+const rejectedName = addKeyword("REJECTED_NAME").addAnswer(
+  "🤔 No te preocupes, ¡lo resolveremos juntos! Por favor, proporciona tu nombre nuevamente para asegurarnos de que esté correcto esta vez. 😊👍",
+  { capture: true },
+  async (ctx, { gotoFlow }) => {
+    if (ctx.body) {
+      return gotoFlow(afterTakeName);
+    }
+  },
+  [afterTakeName]
+);
+
+const flowWelcome = addKeyword("hola")
+  .addAnswer(
+    "🌈 ¡En este viaje de aprendizaje, estaré siempre a tu lado! 🐾💃 Soy uno de los personajes de Musa, pero en esta ocasión, llevo los colores de *atingi*. 🌟💼 🐾💃 \n\n¿Por qué me estoy pintando de los colores de atingi? Bueno, es porque en este viaje educativo, Musa y atingi se han unido para brindarte lo mejor de ambos mundos: la creatividad y la diversión de Musa con la potencia y el conocimiento de atingi. 🌟💼",
+    {
+      capture: true,
+      buttons: [{ body: "¡GENIAL!" }],
+    }
+  )
+  .addAnswer(
+    "📱 Para que estemos más conectad@s, no olvides guardar este número en tus contactos. Ah, y por cierto, *¿cómo te llamas?* Me encantaría conocerte mejor. 👀",
+    {
+      capture: true,
+    }
+  )
+  .addAnswer(
+    `😁 ¡Muchas gracias, undefined! A partir de ahora, usaré ese nombre para dirigirme a ti. ¿Te parece bien?`,
+    {
+      capture: true,
+      buttons: [{ body: "SI" }, { body: "NO" }],
+    },
+    async (ctx, { gotoFlow }) => {
+      const acceptName = ctx.body;
+
+      if (acceptName === "NO") {
+        return gotoFlow(rejectedName);
+      }
+    }
+  )
+  .addAnswer(
+    "¡Excelente! DENTRO😄 Ahora, tengo dos preguntas para registrar tus datos y poder enviarte tu constancia de participación al finalizar esta sesión de aprendizaje.",
+    {
+      capture: true,
+      buttons: [{ body: "OK" }],
+    }
+  )
+  .addAnswer(
+    "💁‍♂️💁‍♀️ Dime, ¿con qué género te identificas?\n\n*A.* Femenino\n*B.* Masculino\n*C.* Otro\n*D.* Prefiero no decirlo\n\n✍️ *Escribe una letra entre A y D.*",
+    {
+      capture: true,
+    }
+  )
+  .addAnswer(
+    "😊💬 ¡Qué emoción! Ahora que conozco un poco más sobre ti, estoy feliz de embarcarnos juntos en esta emocionante aventura de aprendizaje",
+    {
       buttons: [{ body: "¡EMPECEMOS!" }],
       capture: true,
     }
@@ -136,50 +173,50 @@ const flowWelcome = addKeyword("hola")
   .addAnswer(
     "Claro que sí. ¡Bienvenid@ a esta sesión!",
     null,
-    async (ctx, { flowDynamic, gotoFlow }) => {
-      await gotoFlow(require("./app/mainFlow"));
+    async (ctx, { gotoFlow }) => {
+      return gotoFlow(mainFlow);
     },
-    null
+    [rejectedName, afterTakeName, mainFlow]
   );
 
-const mainFlow = addKeyword("MAIN_FLOW")
-  .addAnswer("*EXPLORANDO EL MUNDO DEL MARKETING DIGITAL* 🌐")
-  .addAnswer(
-    "⏱️ *Duración promedio de la sesión* \n10 minutos\n\n🤖 *Tip atingi:* \n▪️Puedes retomar el aprendizaje en cualquier momento\n\n📧 *Correo para consultas:* \nsoporte@holamusa.com",
-    { buttons: [{ body: "OK" }] }
-  );
+// const flowDiscord = addKeyword(["discord"]).addAnswer(
+//   [
+//     "🤪 Únete al discord",
+//     "https://link.codigoencasa.com/DISCORD",
+//     "\n*2* Para siguiente paso.",
+//   ],
+//   null,
+//   null,
+//   [flowSecundario]
+// );
 
-const rejectedName = addKeyword("REJECTED_NAME").addAnswer(
-  "🤔 No te preocupes, ¡lo resolveremos juntos! Por favor, proporciona tu nombre nuevamente para asegurarnos de que esté correcto esta vez. 😊👍",
-  { capture: true },
-  null,
-  [flowWelcome]
-);
+// const flowPrincipal = addKeyword(["principal"])
+//   .addAnswer("🙌 Hola bienvenido a este *Chatbot*")
+//   .addAnswer(
+//     [
+//       "te comparto los siguientes links de interes sobre el proyecto",
+//       "👉 *doc* para ver la documentación",
+//       "👉 *gracias*  para ver la lista de videos",
+//       "👉 *discord* unirte al discord",
+//     ],
+//     null,
+//     null,
+//     [flowDocs, flowGracias, flowTuto, flowDiscord, rejectedName]
+//   );
 
-const flowDiscord = addKeyword(["discord"]).addAnswer(
-  [
-    "🤪 Únete al discord",
-    "https://link.codigoencasa.com/DISCORD",
-    "\n*2* Para siguiente paso.",
-  ],
-  null,
-  null,
-  [flowSecundario]
-);
-
-const flowPrincipal = addKeyword(["principal"])
-  .addAnswer("🙌 Hola bienvenido a este *Chatbot*")
-  .addAnswer(
-    [
-      "te comparto los siguientes links de interes sobre el proyecto",
-      "👉 *doc* para ver la documentación",
-      "👉 *gracias*  para ver la lista de videos",
-      "👉 *discord* unirte al discord",
-    ],
-    null,
-    null,
-    [flowDocs, flowGracias, flowTuto, flowDiscord]
-  );
+// const flujoFinal = addKeyword("imagen").addAnswer(
+//   "Hola!",
+//   null,
+//   async (ctx, { flowDynamic }) => {
+//     await flowDynamic([
+//       {
+//         body: "SOY UNA IMAGEN",
+//         media: "https://i.imgur.com/1XEiIGq.png",
+//         delay: 500,
+//       },
+//     ]);
+//   }
+// );
 
 const main = async () => {
   const adapterDB = new MockAdapter();
@@ -187,7 +224,7 @@ const main = async () => {
 
   const adapterProvider = createProvider(MetaProvider, {
     jwtToken:
-      "EAAO7cfcvkY4BOz5D0QqZCQMYEGYs7ZB5mmsxBbdjDqAiB1VXeWAI5ujXHqaPNv8Xixqyjw01YeKZC5BEAwZCpQwAarx9Mip1o4wFinEq3bLmz1znmXZBXIVMGbpXMiXjloJszmnwk09eFc8ShIY2CN6fx2HBz5ry3ZBgV9MXNrmRHdK19BEvdifNpFvEa78G9FaXfOmKhm16jHl00ZD",
+      "EAAO7cfcvkY4BO9uZBBmmMiOLliGEzuwZAnJiDW3qEAmVqJlsMDIuVrbx4IZBm56DTNOqujzF01LYZBBYjMrGS7fowwZCpMS5Pa4211KdUBkLZC7m3DJZBenpM36R9FxltCZCJgi6jwRxV421wfhDTQe6nKbKU0DG79CpfTUbiAAa9S2JZAQE7dHz4E6rh75xfaOAxTH68RYSFcZBrBxIoZD",
     numberId: "244396062094814",
     verifyToken: "loquesea",
     version: "v16.0",
